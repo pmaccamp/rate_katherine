ccddf43dc8e1c36efbc6709da8d525f98480ef36
Patrick McKeown <pmaccamp@sbcglobal.net>
2016-12-08 23:55:32 -0800

    fix wsgi

--- ratekatherine/wsgi.py
+++ ratekatherine/wsgi.py
@@ -1,5 +1,11 @@
-from django.core.wsgi import get_wsgi_application
+import os
+import django
+
 from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ratekatherine.settings")
+
+django.setup()
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
